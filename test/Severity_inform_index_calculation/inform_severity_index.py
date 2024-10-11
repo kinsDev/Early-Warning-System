1e67d1b86297904b19364cb3a526823db536efa3
Viktor Ivanenko <109345462+xxxVIKTORxxx@users.noreply.github.com>
2024-10-11 11:01:38 +0300

    Update inform_severity_index.py

--- test/Severity_inform_index_calculation/inform_severity_index.py
+++ test/Severity_inform_index_calculation/inform_severity_index.py
@@ -1,4 +1,16 @@
-
+import math as m
 
 def INFORM_SEVERITY_INDEX(impact_of_the_crises, conditions_of_people_affected, complexity_of_the_cirsis):
-    agg_impact = impact_of_the_crises * 33%
+    agg_impact = impact_of_the_crises * 33/100
+    agg_conditions = conditions_of_people_affected *66/100
+    agg_complexity = complexity_of_the_cirsis * 30/100
+
+    geom_avg = m.sqrt(agg_impact*agg_conditions)
+
+    agg_geom_avg = geom_avg * 70/100
+
+    sum = agg_geom_avg + agg_complexity
+
+    inform_severity_index = sum
+
+    return inform_severity_index
