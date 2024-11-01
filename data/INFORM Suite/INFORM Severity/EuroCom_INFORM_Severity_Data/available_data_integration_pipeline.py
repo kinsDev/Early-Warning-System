4a03565e7ea0a40a1e4a25a0d01e15cb609d964d
Raj <ketakishah369@gmail.com>
2024-11-02 00:47:09 +0530

    rename, move, added feature selection list and some minor changes

--- data/INFORM Suite/INFORM Severity/EuroCom_INFORM_Severity_Data/available_data_integration_pipeline.py
+++ data/INFORM Suite/INFORM Severity/EuroCom_INFORM_Severity_Data/available_data_integration_pipeline.py
@@ -12,11 +12,10 @@ def final_pipeline():
     # fetch_new_eurocom_data() -- In the final realtime pipeline, it will only fetch the latest monthly data.
     # Once the latest data is fetched, it will move the merger notebook file from the folder of previous month to the folder of the latest month. This assumes that the monthly merger file for the previous month will work for the latest monthly data as well. If any structural changes in the monthly  releases are made then this might create problems while merging the monthly data.
     # copy_file()
-    years = [i for i in range(2020,2023)]
+    years = [i for i in range(2020,2025)]
     for year in years:
 
         run_main(year)
         copy_main(year)
         integrate_main()
         
-final_pipeline()
